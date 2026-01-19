6a81ecb38b02af569c212b9e78d5902a1a424a98
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 11:54:46 -0500

    chore: converts to new prompt v4 system etc (#11)
    * chore: converts to new prompt v4 system etc
    
    * fix

--- lib/cli/docker.sh
+++ lib/cli/docker.sh
@@ -25,7 +25,7 @@ p6df::modules::oracle::cli::docker::run() {
 
   local now_eps=$(p6_dt_now_epoch_seconds)
 
-  docker run -d --name oracle-${now_eps} -e 'ACCEPT_EULA=Y' -e 'ORACLE_PWD=$ORACLE_PWD' -p 11521:11521 oracle/database:18.4.0-xe
+  docker run -d --name "oracle-${now_eps}" -e 'ACCEPT_EULA=Y' -e 'ORACLE_PWD=$ORACLE_PWD' -p 11521:11521 oracle/database:18.4.0-xe
 
   p6_return_void
 }
